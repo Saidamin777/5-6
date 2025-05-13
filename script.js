e6825d5ae59d4edb611dfdaf763e4abc96e9b514
Saidamin777 <saidakmalov1707@gmail.com>
2025-05-13 09:47:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// const sonlar = [123, 412, 23, 4, 5, 1, 2, 5, 7, 234, 2, 145, 6, 232, 1, 54, 235634, 2, 31, 2, 3, 2356, 2, 232, 23,]
+
+// sonlar.forEach(element => {
+//     console.log(element);
+
+//     if (condition) {
+//         const Juft = []
+
+//     } else {
+//         const Toq = []
+
+//     }
+
+// });
+
+
+
+
+// let newarr = arr.concat(arr2)
+// let newarr = arr.includes('12')
+// let newarr = arr.length
+// let newarr = arr.join('')
+
+// arr.push('qoshildim', 22)
+// arr.unshift('jonka')
+// arr.shift()
+// arr.shift()
+// arr.shift()
+// arr.pop()
+// arr.pop()
+// arr.pop()
+
+
+const obj = ['olma', 'banana', 'gilos', 'shaftoli']
+
+const arr = ['olma', 'banana', 'gilos', 'shaftoli']
+
+console.log(obj);
+
+alert(`boshlanishida arrayning uzunligi: ${obj.length}`)
+
+let result = confirm(`sizning arrayingizdan malumotlarni olib tashlamoqchimisiz`)
+
+if (result) {
+    arr.pop()
+    console.log(arr);
+    
+} else {
+    console.log(arr);
+    
+}
+
+alert(`arrayning uzunligi ${arr.length}`)
+
+
